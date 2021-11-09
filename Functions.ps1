e1e8f24330d6fed65f5deca86479046b64f0c827
isomer04 <43922158+isomer04@users.noreply.github.com>
2021-11-09 13:48:33 -0500

    Add files via upload

---
+++ Functions.ps1
@@ -0,0 +1,36 @@
+﻿#Set-ExecutionPolicy Unrestriced
+clear
+
+
+function testFunction{
+    $number = 15
+ 
+    "This number is " + $number
+
+    if($number -gt 5 -and $number -lt 20){
+        "The Number is greater than 5 and less than 20"
+    } elseif($number -gt 10) {
+        "the  Number is greater than 10, but less than 20"
+    }
+    else{
+
+        "The number is not greater than 10"
+    }
+
+
+}
+
+testFunction
+
+
+function loopTest{
+    $val = 0
+    while($val -ne 10){
+        $val++
+        $val
+    }
+
+
+}
+
+loopTest
---
+++ JuiceQuantity.ps1
@@ -0,0 +1,27 @@
+﻿clear
+
+#1 instock equals true
+#2 instock equals false
+
+function getJuice($inStock){
+    
+    
+    $currentDollars = 4
+    $juiceQuantity = 0
+
+    if($inStock -eq 1){
+        While($currentDollars -gt 0){
+            $currentDollars = $currentDollars -2
+            $juiceQuantity = $juiceQuantity + 1
+            "I have " + $juiceQuantity + " bottles of juice!"
+            }
+            
+        } else {
+            "There is no juice, I am going home!"
+        }
+    
+
+}
+
+
+getJuice(1)
